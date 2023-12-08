cfeeaa828e90e4e0f6a31f5aa1cf015f52c1da90
me996 <me996@gmail.com>
2023-12-08 18:37:42 +0100

    storage_update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,6 +6,13 @@ and deserializes JSON file to instances
 import json 
 from models.base_model import BaseModel
 from os import path
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
 
 
 class FileStorage:
