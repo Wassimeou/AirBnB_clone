13c58f4ca49ee0bd665c354e6ffd19a5ff74b754
me996 <me996@gmail.com>
2023-12-09 12:38:02 +0100

    pycodestyle

--- models/review.py
+++ models/review.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """Defines the Review class."""
 from models.base_model import BaseModel
-
-
 class Review(BaseModel):
     """Represent a review.
     Public class attributes:
@@ -10,6 +8,7 @@ class Review(BaseModel):
         user_id : string - The User id.
         text : string - The text of the review.
     """
+
     place_id = ""
     user_id = ""
     text = ""
